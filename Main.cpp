80e4e3fd7140df46c459b6209343cc95f794f0ce
Leo-Carroll <leocarroll101@gmail.com>
2026-02-12 10:29:12 -0500

    Refactor Person and Book structures for clarity

--- Main.cpp
+++ Main.cpp
@@ -1,71 +1,75 @@
 #include <iostream>
 
-constexpr size_t NUM_MAX_BOOKS = 10;
+constexpr std::size_t MAX_BOOKS_WRITTEN = 10;
 
 struct Book;
 
 struct Person {
-	Book* booksWritten[NUM_MAX_BOOKS];
+	Book* booksWritten[MAX_BOOKS_WRITTEN];
 	std::string name;
 
-	Person(Book*[NUM_MAX_BOOKS], size_t, const std::string&);
+	Person(Book*[MAX_BOOKS_WRITTEN], const std::string&);
 };
 
 struct Book {
 	Person* author;
 	std::string title;
-	uint32_t numberOfPages;
+	std::uint32_t numberOfPages;
 
-	Book(Person*, const std::string&, uint32_t);
+	Book(Person*, const std::string&, std::uint32_t);
 };
 
-Person::Person(Book* books[NUM_MAX_BOOKS], size_t numStartingBooks, const std::string& name) {
-	for (size_t i = 0; i < numStartingBooks; ++i) {
-		if (books[i]) {
-			this->booksWritten[i] = books[i];
+Person::Person(Book* books[MAX_BOOKS_WRITTEN], const std::string& name) {
+	std::size_t idx = 0;
+	if (books != nullptr) {
+		while (books[idx] != nullptr) {
+			this->booksWritten[idx] = books[idx++];
 		}
 	}
-	for (size_t i = numStartingBooks; i < NUM_MAX_BOOKS; ++i) {
-		this->booksWritten[i] = nullptr;
+	for (std::size_t i = idx; i < MAX_BOOKS_WRITTEN; ++i) {
+		this->booksWritten[i] = {};
 	}
 
 	this->name = name;
 }
 
-Book::Book(Person* author, const std::string& title, uint32_t pages) {
+Book::Book(Person* author, const std::string& title, std::uint32_t pages) {
 	this->author = author;
 	this->title = title;
 	this->numberOfPages = pages;
+
 	if (this->author) {
 		size_t idx = 0;
-		while (author->booksWritten[idx++] != nullptr);
-		author->booksWritten[idx - 1] = this;
+		while (this->author->booksWritten[idx]) ++idx;
+		this->author->booksWritten[idx] = this;
 	}
 }
 
-std::ostream& operator<<(std::ostream& os, const Book& book) {
-	os << book.title << ", " << (book.author != nullptr ? book.author->name : "Unknown") << ", " << book.numberOfPages;
-	return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const Person& author) {
-	os << author.name << '\n';
-	size_t idx = 0;
-	while (author.booksWritten[idx] != nullptr) {
-		os << " - " << *author.booksWritten[idx] << (author.booksWritten[idx + 1] != nullptr ? "\n" : "");
-		++idx;
-	}
-	return os;
-}
+std::ostream& operator<<(std::ostream&, const Book&);
+std::ostream& operator<<(std::ostream&, const Person&);
 
 int main() {
-	Person king({}, 0, "Stephen King");
-	Person tolkien({}, 0, "J.R.R. Tolkien");
+	Person king({}, "Stephen King");
+	Person tolkien({}, "J.R.R. Tolkien");
 
 	Book book1(&king, "It", 1024);
 	Book book2(&king, "The Shining", 976);
 	Book book3(&king, "Cujo", 450);
-	Book book4(&tolkien, "Lord of the Rings: Fellowship of the Ring", 512);
+	Book book4(&tolkien, "The Lord of the Rings: Fellowship of the Ring", 512);
 
-	std::cout << king << '\n' << tolkien << '\n';
+	std::cout << king << "\n" << tolkien;
+}
+
+std::ostream& operator<<(std::ostream& os, const Book& book) {
+	os << book.title << ", " << (book.author ? book.author->name : "Unknown") << ", " << book.numberOfPages << " pages";
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Person& person) {
+	os << person.name;
+	size_t idx = 0;
+	while (person.booksWritten[idx] != nullptr) {
+		os << "\n - " << *person.booksWritten[idx++];
+	}
+	return os;
 }
